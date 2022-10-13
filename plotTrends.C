57b53e64577c97e80cddcd9962a9686bdbda751e
Oleg Golosov <oleg.golosov@gmail.com>
2022-10-14 00:07:23 +0300

    make trend-plotter work with monitoring histograms from bmnroot

--- plotTrends.C
+++ plotTrends.C
@@ -1,22 +1,29 @@
-vector <TFile*> makeFileVector (string fileListPath);
-vector <string> buildObjectList (TDirectory *folder, int depth=1);
-vector <float> getRunIds(vector<TFile*> files);
-vector <float> getRunTimes(vector<TFile*> files);
-void plot(vector <TFile*> files, vector<string> histnames, vector<float> xAxis, const char *xAxisTitle, bool isTime, TDirectoryFile *outDir);
+vector <TFile*> makeFileVector (string &fileListPath);
+vector <string> buildObjectList (vector <string> &histNames, TDirectory *folder, int depth=1);
+void filterObjectList (vector <string> &objectList, string &pattern);
+vector <float> getRunIds(vector<TFile*> &files, vector <string> &histNames, string &pattern);
+vector <float> getRunTimes(vector<TFile*> &files);
+void plot(vector <TFile*> &files, vector<string> &histnames, vector<float> &xAxis, const char *xAxisTitle, bool isTimeGraph, TDirectoryFile *outDir);
 
-void plotTrends(string fileListPath="list", const char *outFilePath="trends.root")
+void plotTrends(string fileListPath="list", string pattern="ref(\\d+)_GEM.*", const char *outFilePath="trends.root")
 {
   vector <TFile*> files=makeFileVector(fileListPath);
-  vector <string> histNames=buildObjectList(files.at(0));
-  vector <float> runIds = getRunIds(files);
-  vector <float> runTimes = getRunTimes(files);
+  vector <string> histNames;
+  buildObjectList(histNames, files.at(0));
+  vector <float> runIds;
+  vector <float> runTimes;
+  runIds = getRunIds(files, histNames, pattern);
+  if (pattern.find("(") > pattern.size()) // explicit info stored in file
+    runTimes = getRunTimes(files);
+  filterObjectList(histNames, pattern);
   auto outFile = new TFile(outFilePath, "recreate");
   plot(files, histNames, runIds, "runId", false, outFile);
-  plot(files, histNames, runTimes, "time", true, outFile);
+  if (runTimes.size()>0)
+    plot(files, histNames, runTimes, "time", true, outFile);
   outFile->Close();
 }
 
-void plot(vector <TFile*> files, vector<string> histNames, vector<float> xAxis, const char *xAxisTitle, bool isTime, TDirectoryFile *outDir)
+void plot(vector <TFile*> &files, vector<string> &histNames, vector<float> &xAxis, const char *xAxisTitle, bool isTimeGraph, TDirectoryFile *outDir)
 {
   printf ("\nPlotting %s trends:\n", xAxisTitle);
   outDir->mkdir(xAxisTitle);
@@ -24,8 +31,13 @@ void plot(vector <TFile*> files, vector<string> histNames, vector<float> xAxis, 
   for (auto histName:histNames)
   {
     cout << histName << endl;
-    auto templateHist=(TH1*)files.at(0)->Get(histName.c_str());
-    const char *histTitle=templateHist->GetTitle();
+    auto writeName=histName;
+    while (writeName.find("%")<writeName.size())
+      writeName.erase(writeName.find("%"),writeName.find("i",writeName.find("%"))-writeName.find("%")+1);
+    while (writeName.find("/")<writeName.size())
+      writeName.replace(writeName.find("/"),1,"_");
+    auto templateHist=(TH1*)files.at(0)->Get(Form(histName.c_str(), int(xAxis.at(0)), int(xAxis.at(0))));
+    auto histTitle=templateHist->GetTitle();
     string yAxisTitle=templateHist->GetXaxis()->GetTitle();
     uint commaPosition=yAxisTitle.rfind(",");
     string units;
@@ -39,9 +51,15 @@ void plot(vector <TFile*> files, vector<string> histNames, vector<float> xAxis, 
     {
       vector <float> trend;
       auto moment=moments.at(i);
-      for (auto file:files)
+      for (int f=0;f<files.size();f++)
       {
-        auto h=(TH1*)file->Get(histName.c_str());
+        auto file=files.at(f);
+        auto h=(TH1*)file->Get(Form(histName.c_str(), int(xAxis.at(f)), int(xAxis.at(f))));
+        if (!h) 
+        {
+          printf("\nNot found in file %s\n", file->GetName());
+          continue;
+        }
         if (moment=="mean") 
           trend.push_back(h->GetMean()); 
         else if (moment=="RMS") 
@@ -54,8 +72,8 @@ void plot(vector <TFile*> files, vector<string> histNames, vector<float> xAxis, 
       }
       auto g=new TGraph(files.size(), &xAxis[0], &trend[0]);
       g->SetTitle(Form("%s (%s);%s;%s %s%s", histTitle, moment.c_str(), xAxisTitle, yAxisTitle.c_str(), moment.c_str(), units.c_str()));
-      g->SetName(Form("%s_%s", histName.c_str(), moment.c_str()));
-      if (isTime) 
+      g->SetName(Form("%s_%s", writeName.c_str(), moment.c_str()));
+      if (isTimeGraph) 
       {
         g->GetXaxis()->SetTimeDisplay(1);
         g->GetXaxis()->SetTimeFormat("%d/%m-%H:%M:%S");
@@ -66,18 +84,18 @@ void plot(vector <TFile*> files, vector<string> histNames, vector<float> xAxis, 
   }
 }
 
-vector <TFile*> makeFileVector (string fileListPath)
+vector <TFile*> makeFileVector (string &fileListPath)
 {
   cout << "\nFile list:\n";
   vector <TFile*> files;
-  if(fileListPath.rfind(".root") == fileListPath.size()-5) // root file
+  if(fileListPath.rfind(".root") < fileListPath.size()) // root file
     files.push_back(new TFile(fileListPath.c_str()));
   else // file list
   {
     ifstream fileList;
     fileList.open(fileListPath);
     if(!fileList)
-      printf("File %s does not exist\n", fileListPath.c_str());
+      printf("Error opening file: %s\n", fileListPath.c_str());
     string name;
     while(getline(fileList, name))
     {
@@ -89,18 +107,30 @@ vector <TFile*> makeFileVector (string fileListPath)
   return files;
 }
 
-vector <float> getRunIds(vector<TFile*> files)
+vector <float> getRunIds(vector<TFile*> &files, vector <string> &histNames, string &pattern)
 {
   vector <float> runIds;
   for (auto f:files)
   { 
-    auto runIdVector=(vector<ulong>*)f->Get("runId");
-    runIds.push_back(runIdVector->at(0));
+    if (pattern.find("(") < pattern.size())
+    {
+      smatch sm;
+      string name=f->GetListOfKeys()->At(0)->GetName();
+      regex_search(name, sm, regex(pattern));
+      string id=sm[1];
+      runIds.push_back(stoi(id));
+    }
+    else
+    {
+      auto runIdVector=(vector<ulong>*)f->Get("runId");
+      for (auto &name:histNames)
+      runIds.push_back(runIdVector->at(0));
+    }
   }
   return runIds;
 }
 
-vector <float> getRunTimes(vector<TFile*> files)
+vector <float> getRunTimes(vector<TFile*> &files)
 {
   vector <float> runTimes;
   for (auto f:files)
@@ -118,17 +148,39 @@ vector <float> getRunTimes(vector<TFile*> files)
   return runTimes;
 }
 
-vector <string> buildObjectList (TDirectory *folder, int depth)
+void filterObjectList (vector <string> &objectNames, string &pattern)
+{
+  if (pattern.size()==0)
+    return;
+  cout << "\nFiltering object list\n";
+  regex re(pattern);
+  vector <string> filteredNames;
+  for (auto &name:objectNames)
+  {
+    smatch sm;
+    if(regex_search(name, sm, regex(pattern)))
+    {
+      string capture=sm[1];
+      while(name.find(capture.c_str())<name.size())
+        name.replace(name.find(capture.c_str()), capture.size(), Form("%s%lui", "%0", capture.size()));
+      cout << name << endl;
+      filteredNames.push_back(name);
+    }
+  }
+  swap(filteredNames, objectNames);
+  return;
+}
+
+vector <string> buildObjectList (vector <string> &objectNames, TDirectory *folder, int depth)
 {
   int maxDepth=10000;
-  vector<string> excludedFolders;
-  vector <string> objectNames;
   string folderPath = folder -> GetPath();
   folderPath.erase (0,folderPath.rfind(":/")+2);
   while (folderPath.find("/")==0)
     folderPath.erase (0,1);
   TList *keys = folder -> GetListOfKeys ();
-  cout << "\nBuilding object list:\n";
+  if (depth==1)
+    cout << "\nBuilding object list:\n";
   for (auto key : *keys)
   {
     string objectName = key -> GetName();
@@ -137,7 +189,7 @@ vector <string> buildObjectList (TDirectory *folder, int depth)
     auto object = dynamic_cast <TKey*> (key) -> ReadObj ();
     string className = object -> ClassName();
     if (!(className.find("3") < className.size()) && 
-      (className.find("TH")==0 || className.find("TProfile")==0 || className.find("Graph")==0))
+      (className.find("TH1")==0 || className.find("TProfile")==0 || className.find("Graph")==0))
     {
       string objectPath=Form("%s/%s", folderPath.c_str(), objectName.c_str());
       if(objectPath.find("/")==0)
@@ -146,13 +198,10 @@ vector <string> buildObjectList (TDirectory *folder, int depth)
       objectNames.push_back(objectPath);
     }
     else if (className == "TDirectoryFile" && depth < maxDepth)
-    {
-      bool skipFolder = false;
-      for (auto exFolder : excludedFolders)
-        if (objectName == exFolder) skipFolder = true;  
-      if (!skipFolder) buildObjectList (dynamic_cast <TDirectoryFile*> (object), depth + 1);
-    }
+      buildObjectList (objectNames, dynamic_cast <TDirectoryFile*> (object), depth + 1);
     delete object;
   }
+  if (depth==1)
+    cout << objectNames.size() << " objects added.\n";
   return objectNames; 
 }
